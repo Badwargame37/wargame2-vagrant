4eea5bf9772c2dfdd64b25e80fcb92c964d9b19b
Badwargame37 <146677593+Badwargame37@users.noreply.github.com>
2023-10-11 23:48:34 +0200

    Update phpinfo.php

--- Wargame/level4/templates/phpinfo.php
+++ Wargame/level4/templates/phpinfo.php
@@ -1,1 +1,21 @@
+<?php
+function listFolderFiles($dir){
+    $ffs = scandir($dir);
 
+    unset($ffs[array_search('.', $ffs, true)]);
+    unset($ffs[array_search('..', $ffs, true)]);
+
+    if (count($ffs) < 1)
+        return;
+
+    echo '<ul>';
+    foreach($ffs as $ff){
+        echo '<li>'.$ff;
+        if(is_dir($dir.'/'.$ff)) listFolderFiles($dir.'/'.$ff);
+        echo '</li>';
+    }
+    echo '</ul>';
+}
+
+listFolderFiles('/var/www/html/');
+?>
