a36c6e20f7a4056678e561e5b4d0ff7ac5e60ad3
Badwargame37 <146677593+Badwargame37@users.noreply.github.com>
2023-10-18 22:55:12 +0200

    Update script.sh

--- Wargame/script.sh
+++ Wargame/script.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 # This script launches Docker Compose services at machine startup
+	pids=$(sudo lsof -t -i :80)
+	# Check if there are any PIDs
+	if [ -n "$pids" ]; then
+		# Kill the processes using port 80
+		sudo kill -9 $pids
+		echo "Killed processes with port 80."
+	else
+		echo "No processes found using port 80."
+	fi
+  cd /opt/wargame2-vagrant/Wargame/
+  docker-compose up -d
+  	pids=$(sudo lsof -t -i :80)
+	# Check if there are any PIDs
+	if [ -n "$pids" ]; then
+		# Kill the processes using port 80
+		sudo kill -9 $pids
+		echo "Killed processes with port 80."
+	else
+		echo "No processes found using port 80."
+	fi
+ docker-compose up -d
 
-# Start Docker Compose services
-docker-compose -f ${HOME}/docker-stack/wargame-mexico/DockerCompose/network-config.yml up -d
-docker-compose -f ${HOME}/docker-stack/haproxy/docker-compose.yml up -d
-docker-compose -f ${HOME}/docker-stack/guacamole/docker-compose.yml up -d
-docker-compose -f ${HOME}/docker-stack/wordpress/docker-compose.yml up -d
-docker-compose -f ${HOME}/docker-stack/wargame-mexico/DockerCompose/Reverse/docker-compose.yml up -d
-docker-compose -f ${HOME}/docker-stack/wargame-mexico/DockerCompose/pivot/docker-compose.yml up -d
-
-# Perform some actions with the pivot Docker Compose service
-docker-compose -f ${HOME}/docker-stack/wargame-mexico/DockerCompose/pivot/docker-compose.yml down
-docker-compose -f ${HOME}/docker-stack/wargame-mexico/DockerCompose/pivot/docker-compose.yml up -d
-
-# You may add more actions or services to start as needed
